dbb3ba1a3409bb44826191fd0405ec1462160188
ntsvetkov <ntsvetkov@npoprogress.com>
2016-06-16 18:09:20 +0300

    Completed sixth chapter

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,17 +1,44 @@
 import { Component }       from '@angular/core';
 import { HeroService } from './hero.service'
+import { DashboardComponent } from './dashboard.component'
 import { HeroesComponent } from './heroes.component';
+import { HeroDetailComponent } from './hero-detail.component';
+
+import { RouteConfig, ROUTER_DIRECTIVES, ROUTER_PROVIDERS } from '@angular/router-deprecated';
+
 @Component({
   selector: 'my-app',
   template: `
     <h1>{{title}}</h1>
-    <my-heroes></my-heroes>
+    <nav><a [routerLink]="['Heroes']">Heroes</a>
+    <a [routerLink]="['Dashboard']">Dashboard</a></nav>
+    <router-outlet></router-outlet>
   `,
-  directives: [HeroesComponent],
+  styleUrls: ['app/app.component.css']
+  directives: [ROUTER_DIRECTIVES],
   providers: [
+    ROUTER_PROVIDERS,
     HeroService
   ]
 })
+@RouteConfig([
+  {
+    path: '/heroes',
+    name: 'Heroes',
+    component: HeroesComponent
+  },
+  {
+    path: '/dashboard',
+    name: 'Dashboard',
+    component: DashboardComponent,
+    useAsDefault: true
+  },
+  {
+    path: '/detail/:id',
+    name: 'HeroDetail',
+    component: HeroDetailComponent
+  }
+])
 export class AppComponent {
   title = 'Tour of Heroes';
 }
